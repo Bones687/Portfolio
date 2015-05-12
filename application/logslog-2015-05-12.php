4375d3f5ad31f4c99b3630bb93c4b90b127c3eef
Bones687 <neildalton6@gmail.com>
2015-05-12 15:53:14 -0600

    Codeigniter framework added

---
+++ application/logslog-2015-05-12.php
@@ -0,0 +1,136 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2015-05-12 13:03:27 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 13:03:53 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 13:04:51 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 13:06:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:24 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:06:27 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:06:29 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:06:33 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:06:37 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:37 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:37 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:37 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:37 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:37 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:37 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:37 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:06:37 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:06:51 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 13:11:04 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:04 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:04 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:04 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:04 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:04 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:04 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:04 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:04 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:11:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:11:24 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:11:30 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:11:33 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:11:41 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:14:55 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:14:55 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:14:55 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:14:55 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:14:55 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:14:55 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:14:55 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:14:55 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 13:14:55 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 13:15:59 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 13:23:50 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:33:15 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:44:10 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:44:10 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:44:10 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:44:10 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:44:10 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:44:11 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:45:52 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:45:52 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:45:52 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:45:52 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:45:52 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:51:24 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:24 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:24 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:24 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:24 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:24 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:51:32 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:32 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:32 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:32 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:32 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:51:32 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:51:54 --> 404 Page Not Found: Images/thumbs
+ERROR - 2015-05-12 14:51:54 --> 404 Page Not Found: Images/fulls
+ERROR - 2015-05-12 14:51:54 --> 404 Page Not Found: Images/thumbs
+ERROR - 2015-05-12 14:51:54 --> 404 Page Not Found: Images/thumbs
+ERROR - 2015-05-12 14:51:54 --> 404 Page Not Found: Images/thumbs
+ERROR - 2015-05-12 14:51:54 --> 404 Page Not Found: Images/thumbs
+ERROR - 2015-05-12 14:51:54 --> 404 Page Not Found: Images/avatar.jpg
+ERROR - 2015-05-12 14:51:54 --> 404 Page Not Found: Images/thumbs
+ERROR - 2015-05-12 14:51:54 --> 404 Page Not Found: Css/style.css
+ERROR - 2015-05-12 14:52:04 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:52:04 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:52:04 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:52:04 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:52:04 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 16
+ERROR - 2015-05-12 14:52:04 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:53:03 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 17
+ERROR - 2015-05-12 14:53:03 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 17
+ERROR - 2015-05-12 14:53:03 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 17
+ERROR - 2015-05-12 14:53:03 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 17
+ERROR - 2015-05-12 14:53:03 --> Severity: Notice --> Undefined index: item_type_id /Users/neil/Documents/PortfolioSite/Portfolio/application/models/Model_item.php 17
+ERROR - 2015-05-12 14:53:03 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:53:31 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 14:54:02 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 15:30:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:30:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:30:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:30:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:30:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:30:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:30:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:30:24 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:30:24 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 15:30:50 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 15:36:28 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:36:28 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:36:28 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:36:28 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:36:28 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:36:28 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:36:28 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:36:28 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:36:28 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 15:36:36 --> 404 Page Not Found: Admin/css
+ERROR - 2015-05-12 15:36:56 --> 404 Page Not Found: Welcome/css
+ERROR - 2015-05-12 15:37:00 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:37:00 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:37:01 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:37:01 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:37:01 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:37:01 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:37:01 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:37:01 --> 404 Page Not Found: Welcome/images
+ERROR - 2015-05-12 15:37:01 --> 404 Page Not Found: Welcome/css
---
+++ application/models/model_user.php
@@ -0,0 +1,45 @@
+<?php
+
+class model_user extends CI_Model {
+  public function __construct()
+  {
+    parent::__construct();
+  }
+  function login($email,$password)
+  {
+    $this->db->where("user_email",$email);
+    $this->db->where("user_password",$password);
+
+    $query=$this->db->get("user");
+    if($query->num_rows()>0)
+    {
+      foreach($query->result() as $rows)
+      {
+        //add all data to session
+        $newdata = array(
+          'user_id'  => $rows->user_id,
+          'user_name'  => $rows->user_email,
+          'name' => $rows->user_first_name,
+          'user_type' => $rows->user_type,
+          'user_status' => $rows->user_status,
+          'logged_in'  => TRUE,
+        );
+      }
+      $this->session->set_userdata($newdata);
+      return true;
+    }
+    return false;
+  }
+  public function add_user()
+  {
+    $data=array(
+      'user_email'=>$this->input->post('email_address'),
+      'user_password'=>md5($this->input->post('password')),
+      'user_first_name'=>$this->input->post('user_first_name'),
+      'user_last_name'=>$this->input->post('user_last_name'),
+      'user_address1'=>$this->input->post('user_address1'),
+      'user_address2'=>$this->input->post('user_address2'),
+    );
+    $this->db->insert('user',$data);
+  }
+}
